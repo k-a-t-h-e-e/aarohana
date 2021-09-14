d37aa9ea586e273c43b91b1e9e23b1adb1d750c1
Katheesan <48439616+k-a-t-h-e-e@users.noreply.github.com>
2021-09-14 09:56:31 +0530

    Aarohana Points

---
+++ assets/app.js
@@ -0,0 +1,50 @@
+const youtubeKey = 'AIzaSyC-my0lqjMp9nv3Q3sTtvkXW6sbxCMUcYc';
+const youtubeUser = 'UCOMZV9kTj3mrZgM8WehaB6A';
+const participantList = document.getElementById('participantList');
+
+var points,title;
+
+    const listCreator = (videoID) => {     
+        let titleName = ""
+        fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoID}&key=${youtubeKey}`)
+        .then(response => {    
+            return response.json()
+        })
+        .then(data => {        
+            title=data.items[0].snippet.title;    
+            titleName=title.split(' ').slice(0,2).join(' ')
+            fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoID}&key=${youtubeKey}`)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {    
+        views=parseInt(data.items[0].statistics.viewCount);
+        likes=parseInt(data.items[0].statistics.likeCount);
+        points=2*views + likes;          
+      
+
+        $("#headers1").after("<a target="+"_blank"+" href=https://www.youtube.com/watch?v="+videoID+"><li><mark>"+titleName+"</mark><small>"+points+"</small></li></a>");
+    })               
+                        
+        })
+}
+const playlistID = "PL84zQvV2BhG03MX4erJytCwS4AWwKoK-x"
+const getPlaylist = () => {     
+    fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistID}&maxResults=25&key=${youtubeKey}`)
+    .then(response => {    
+        
+        return response.json();
+    })
+    .then(data => {        
+        console.log(data.items);
+        const items=data.items;
+        items.forEach((i)=>{
+            console.log(i?.snippet?.resourceId?.videoId);
+            listCreator(i?.snippet?.resourceId?.videoId);
+
+    
+        })
+    })
+}
+
+getPlaylist();
